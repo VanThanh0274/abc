354002f48ed6e845e16ed8ad25b2870d394b33c6
VanThanh0274 <thanh.2k4.96@gmail.com>
2026-05-26 15:54:25 +0700

    commit

--- API_CuahangKinhmat/DAL/obj/Debug/net6.0/DAL.AssemblyInfo.cs
+++ API_CuahangKinhmat/DAL/obj/Debug/net6.0/DAL.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DAL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+98fd7db9a62706d8b7c8717bb1a61759ab652728")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+97a800a44341505427c3c36ac4a599fe649c580e")]
 [assembly: System.Reflection.AssemblyProductAttribute("DAL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DAL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
